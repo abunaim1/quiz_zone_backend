e2012e123916b779f4ac2c66765d2cc4136aa962
Abu Naim <abunaim@Abus-MacBook-Air-2.local>
2024-02-27 22:41:58 +0600

    a

--- authentication/models.py
+++ authentication/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from quiz.models import Quiz
 
 class Participant(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, null=True, blank=True)
     def __str__(self):
         return f'{self.user.first_name} {self.user.last_name}'
