d7c07c0dc3e2c1cae1f5d021253845afb6989863
Abu Naim <abunaim@Abus-MacBook-Air-2.local>
2024-02-27 16:11:23 +0600

    permisson cancel

--- question/views.py
+++ question/views.py
@@ -2,19 +2,16 @@ from django.shortcuts import render
 from rest_framework import viewsets, pagination
 from . import models, serializers
 from urllib.parse import urlparse
-from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 
 class QuestionCategoryViewSet(viewsets.ModelViewSet):
     queryset = models.QuestionCategory.objects.all()
     serializer_class = serializers.QuestionCategorySerializer
-    permission_classes = [IsAuthenticated]
 
 class QuestionPaperViewSet(viewsets.ModelViewSet):
     queryset = models.QuestionPaper.objects.all()
     serializer_class = serializers.QuestionPaperSerializer
-    permission_classes = [IsAuthenticated]
 
 class QuestionPagination(pagination.PageNumberPagination):
     page_size = 1
@@ -26,7 +23,6 @@ class QuestionViewSet(viewsets.ModelViewSet):
     queryset = models.Question.objects.all()
     serializer_class = serializers.QuestionSerializer
     pagination_class =  QuestionPagination
-    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         queryset = super().get_queryset()
@@ -36,7 +32,6 @@ class QuestionViewSet(viewsets.ModelViewSet):
 class AnswerOptionViewset(viewsets.ModelViewSet):
     queryset = models.AnswerOption.objects.all()
     serializer_class = serializers.AnswerOptionSerializer
-    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         queryset = super().get_queryset()
