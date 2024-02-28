bb5326a4d274807e96318972a226440ecf843ae8
Abu Naim <abunaim@Abus-MacBook-Air-2.local>
2024-02-28 06:56:33 +0600

    quiz description added

--- quiz/models.py
+++ quiz/models.py
@@ -7,6 +7,7 @@ from rating.models import Rating
 
 class QuizCategory(models.Model):
     category = models.CharField(max_length=100)
+    description = models.CharField(max_length=200, null=True, blank=True)
     slug = models.SlugField(max_length=100, null=True, blank=True)
     def __str__(self):
         return self.category
