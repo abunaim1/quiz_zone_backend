8c5e89a52c3c20fec64e13745e919c112c95cf16
Abu Naim <abunaim@Abus-Air-2.lan>
2024-03-17 16:33:19 +0600

    debug

--- quiz/views.py
+++ quiz/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from rest_framework import viewsets
 from . import models, serializers
+
 # Create your views here.
 
 class QuizCategoryViewSet(viewsets.ModelViewSet):
@@ -10,18 +12,15 @@ class QuizCategoryViewSet(viewsets.ModelViewSet):
 class QuizViewSet(viewsets.ModelViewSet):
     queryset = models.Quiz.objects.all()
     serializer_class = serializers.QuizSerializer
-
     def get_queryset(self):
         queryset = super().get_queryset()
         category_slug = self.request.query_params.get("category_slug")
-                
         if category_slug:
             for quiz in queryset:
                 for cat in quiz.quiz_category.all():
-                    if cat.slug==category_slug:
+                    if cat.category==category_slug:
                         category_slug = cat.id
                         queryset = queryset.filter(quiz_category=category_slug)
-
         return queryset
         
     
