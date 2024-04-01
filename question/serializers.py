33759ba162366c0c130f2445372f38033e0540d6
Abu Naim <abunaim@Abus-Air-2.lan>
2024-04-01 19:04:22 +0600

    answer option little bit changed

--- question/serializers.py
+++ question/serializers.py
@@ -18,7 +18,17 @@ class AnswerOptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = AnswerOption
         fields = '__all__'
-            
+
+    def create(self, validated_data):
+        question_data = validated_data.pop('question', None)
+        answer_option = AnswerOption.objects.create(**validated_data)
+        if question_data:
+            question_name = question_data.get('name')
+            question = Question.objects.get(name=question_name)
+            answer_option.question = question
+        answer_option.save()
+        return answer_option
+
 class QuestionPaperSerializer(serializers.ModelSerializer):
     question_category_name = serializers.CharField(source='question_category.name', read_only=True)
     class Meta:
