07806ffe8f0099084f040fc998725a06011024a2
Abu Naim <abunaim@Abus-Air-2.lan>
2024-05-08 23:33:14 +0600

    add sslcommerz into reequirement.txt file

--- authentication/views.py
+++ authentication/views.py
@@ -71,7 +71,7 @@ def activate(request, uid64, token):
     if user is not None and default_token_generator.check_token(user, token):
         user.is_active = True
         user.save()
-        return redirect('https://quizzonebyabunaim.netlify.app/')
+        return redirect('login')
     else:
         return redirect('registration')
     
